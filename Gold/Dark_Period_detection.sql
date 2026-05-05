c7723926729f4966416c8aa0be8f5e33c92e304d
Rishabh1627rawat <111679332+Rishabh1627rawat@users.noreply.github.com>
2026-05-06 00:44:58 +0530

    Add files via upload

---
+++ Gold/Dark_Period_detection.sql
@@ -0,0 +1,28 @@
+
+-- hop_distance_km already hai — 693 km
+-- Average truck speed = 40 kmph
+
+
+
+-- Kepping the speed as 40 km becasue as realistic to calculate expected_travel_hrs 
+-- expected_travel_hrs = hop_distance_km / 40
+-- 693 / 40 = 17.3 hrs
+
+WITH Dark_Period AS (
+select *,
+      ROUND((HOP_DISTANCE_KM)/40) AS Expected_travel_hrs,
+      TIMESTAMPDIFF('HOUR', NEXT_SCAN_TIMESTAMP, LEAD(SCAN_TIMESTAMP) OVER (PARTITION BY SHIPMENT_ID ORDER BY SCAN_TIMESTAMP)) AS ACTUAL_TOTAL_TIME,
+from shipping_logistic.shipping_gold_schema.shipping_logistic_silver
+
+)
+
+SELECT * ,
+ACTUAL_TOTAL_TIME-Expected_travel_hrs AS EXTRA_HRS,
+CASE 
+    WHEN ACTUAL_TOTAL_TIME > Expected_travel_hrs 
+    THEN 'Dark Period'
+    ELSE 'Normal'
+END AS VISIBILITY_STATUS
+FROM Dark_Period 
+-- ORDER BY EXTRA_HRS DESC
+
