f8f0386567e9d03cd70cf08d9e372131dad5a9ab
Rishabh1627rawat <111679332+Rishabh1627rawat@users.noreply.github.com>
2026-05-08 00:29:41 +0530

    Update Dark_Period_detection.sql

--- Gold/Dark_Period_detection.sql
+++ Gold/Dark_Period_detection.sql
@@ -8,6 +8,8 @@
 -- expected_travel_hrs = hop_distance_km / 40
 -- 693 / 40 = 17.3 hrs
 
+
+
 WITH Dark_Period AS (
 select *,
       ROUND((HOP_DISTANCE_KM)/40) AS Expected_travel_hrs,
@@ -19,10 +21,18 @@ from shipping_logistic.shipping_gold_schema.shipping_logistic_silver
 SELECT * ,
 ACTUAL_TOTAL_TIME-Expected_travel_hrs AS EXTRA_HRS,
 CASE 
+    WHEN HOP_DISTANCE_KM = 0 AND PREV_HUB IS NULL
+    THEN 'Staring Point'
+    when ACTUAL_TOTAL_TIME is null
+    THEN 'Final Delivery'
+    WHEN HOP_DISTANCE_KM = 0
+    THEN 'Same Hub Movement'
     WHEN ACTUAL_TOTAL_TIME > Expected_travel_hrs 
     THEN 'Dark Period'
     ELSE 'Normal'
 END AS VISIBILITY_STATUS
 FROM Dark_Period 
+ 
 -- ORDER BY EXTRA_HRS DESC
+
 
